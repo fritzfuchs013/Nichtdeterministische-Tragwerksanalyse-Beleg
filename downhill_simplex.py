4482abd3d6d85b984255376d2a5b10a8fe447dab
Jochen Diepelt <jochen.diepelt@mailbox.tu-dresden.de>
2023-01-05 17:08:04 +0100

    hinzufügen des grungerüsts für downsim

---
+++ downhill_simplex.py
@@ -0,0 +1,13 @@
+
+import numpy as np
+
+def downhill_simplex(function, parameterbereich: arrayShape):
+    parameter = parameterbereich.len()
+    points = np.zeros((parameter, parameter+1))
+    
+    print("Downhill Simplex Algorithmus")
+
+def main():
+    print("Hello World")
+
+main()
