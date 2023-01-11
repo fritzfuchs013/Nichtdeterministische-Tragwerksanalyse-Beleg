795b8ed673b6d0b9933112c8228c1d532ec20004
fritz_fuchs51 <l_beyerlein@icloud.com>
2023-01-11 15:47:50 +0100

    Aufräumen Main

--- main.py
+++ main.py
@@ -2,52 +2,70 @@ import time
 from matplotlib import pyplot as plt
 from bruteforce import *
 from gausgrit import *
-from genetic import *
+# from genetic import *
 from absuchen import *
 from fuzzy import * 
 
-a = FuzzyTrapez(1, 4, 4, 5)
-x, y = a.giveIntervall(1)
-print(x,y)
-# Eingangsgrößen
-L  = 10
-b  = 0.2
+
+# Initiierung Fuzzy-Analyse -----------------------------------------------------------------
+# Eingabegrößen aus Aufgabenstellung
+L = 10
+b = 0.2
 h1 = FuzzyTrapez(0.65, 0.7, 0.7, 0.75)
 h2 = FuzzyTrapez(0.48, 0.5, 0.5, 0.52)
-E  = FuzzyTrapez(2.05 * (10 ** 8), 2.10 * (10 ** 8), 2.12 * (10 ** 8), 2.15 * (10 ** 8))
-q  = FuzzyTrapez(14.0, 17.0, 18.5, 22.0)
-k  = FuzzyTrapez(0.8 * (10 ** 4), 1.0 * (10 ** 4), 1.0 * (10 ** 4), 1.4 * (10 ** 4))
+E = FuzzyTrapez(2.05 * (10 ** 8), 2.10 * (10 ** 8), 2.12 * (10 ** 8), 2.15 * (10 ** 8))
+q = FuzzyTrapez(14.0, 17.0, 18.5, 22.0)
+k = FuzzyTrapez(0.8 * (10 ** 4), 1.0 * (10 ** 4), 1.0 * (10 ** 4), 1.4 * (10 ** 4))
 
+# Sicherheitsausgabe
 print('program has started')
-#brute force
-# alpha lvl Schleife: (0, 0.5, 1)
+
+
+# brute force --------------------------------------------------------------------------------
+# Beginn Timer
+startTime_bruteforce = time.time()
+
 # Anzahl der Schritte pro Intervall:
 n = 1
-st_bruteforce = time.time()
 
+# Initiierung Algorithmus über alle alpha level in der Schleife: (0, 0.5, 1)
 for alpha in [0.0, 0.5, 1.0]:
     M_max, M_min = bruteforce(L, b, h1, h2, E, q, k, n, alpha)
-    print(M_max, M_min)
 
+    # Kontrolle der einzelnen errechneten Intervalle in der Konsolenausgabe
+    print("-------------------------------------------------------")
+    print('M_max =', M_max)
+    print('M_min =', M_min)
+
+    # Erstellung Plot des jeweiligen alpha-lvl Intervalls
     x_coordinates = [M_min, M_max]
     y_coordinates = [alpha, alpha]
     plt.plot(x_coordinates, y_coordinates, color="navy")
 
-et_bruteforce = time.time()
-elapsed_time_bruteforce = et_bruteforce - st_bruteforce
+# Ende Timer und Bildung Differenz
+endTime_bruteforce = time.time()
+elapsed_time_bruteforce = endTime_bruteforce - startTime_bruteforce
 
-print('M_max =', M_max)
-print('M_min =', M_min)
+# Ausgabe der benötigten Zeit
+print("---------------------------------------------------------------")
+# print()
 print('benötigte Zeit:', elapsed_time_bruteforce)
 
-plt.show()
-plt.savefig("out.png")
+# Ausgabe Plot
+# plt.show()
+plt.savefig("plot_BruteForce.png")
 
-print(genetic_algorithm(L, b, h1, h2, E, q, k, alpha))
+
+# Testen des Darwin-Algorithmus -----------------------------------------------------------------------
+# print(genetic_algorithm(L, b, h1, h2, E, q, k, alpha))
+
+
+# Noch nicht aufgeräumter Test-Krams ------------------------------------------------------------------
+"""
 
 start_time = time.time()
 
-"""for alpha in [0.0, 0.5, 1.0]:
+for alpha in [0.0, 0.5, 1.0]:
     M_min, M_max = strukturiertes_absuchen_grundloesung(L, b, h1, h2, E, q, k, alpha)
     print(alpha, M_min, M_max)
 
@@ -67,4 +85,6 @@ print('benötigte Zeit:', elapsed_time)
 #elapsed_time_gauss = et_gauss - st_gauss
 #print('M_max =', M_max)
 #print('M_min =', M_min)
-#print('benötigte Zeit:', elapsed_time_gauss) """
+#print('benötigte Zeit:', elapsed_time_gauss)
+
+"""
