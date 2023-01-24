551f0b4f996739a185f85799de8190c3d4f89ce3
Chenjox <chenjox.diepelt@gmx.de>
2023-01-24 21:12:45 +0100

    hinzufügen von stochastischen Variablen

---
+++ stochastic.py
@@ -0,0 +1,16 @@
+import numpy as np
+from scipy.stats import norm 
+
+class StochasticVariable:
+  def get_sample(self):
+    return 0.0
+
+class NormaldistributedVariable(StochasticVariable):
+  
+  def __init__(self, erwartungswert, standardabweichung):
+    self.erwartungswert = erwartungswert
+    self.standardabweichung = standardabweichung
+
+  def get_sample(self):
+    rv = norm(erwartungswert, standardabweichung)
+    return rv.rvs()
